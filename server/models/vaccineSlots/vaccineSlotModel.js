68ad518edae028663d936f697234ef76e4efb1ae
Bibhu Prasad <bibhup.behera@nettantra.net>
2022-05-10 18:29:01 +0530

    Modifedd

--- server/models/vaccineSlots/vaccineSlotModel.js
+++ server/models/vaccineSlots/vaccineSlotModel.js
@@ -4,10 +4,10 @@ const vaccineSlotSchema = new mongoose.Schema({
     date : {
         type: String,
     },
-    user:[{
+    user:{
         type: mongoose.Schema.Types.ObjectId,
 		ref: "users"
-    }]
+    }
 });
 
 const VaccineSlot = mongoose.model('vaccineSlots', vaccineSlotSchema);
